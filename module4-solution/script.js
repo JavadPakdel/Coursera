04a05221b0cef981dee92f9b8061760ab5f0f36c
Javad Pakdel <78685118+JavadPakdel@users.noreply.github.com>
2021-09-29 01:46:23 +0330

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,5 +1,5 @@
 (function() {
-    var names = ["sara", "Frank", "Julia", "Jason", "elizabeth", "Larry", "Johnson", "josepe", "veronica", "Laura"];
+    var names = ["Yaakov", "John", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
